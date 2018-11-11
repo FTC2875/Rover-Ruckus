a4bfedab83b0d2275272cbfa5c9d72cbf8003f91
FTC2875 <2875mitibots@gmail.com>
2018-11-11 16:59:52 -0500

    played with demo bot also played around with tensorflow, it's good but don't know if its worth to integrate

--- FtcRobotController/src/main/java/vision/YellowBlockAnaylzer.java
+++ FtcRobotController/src/main/java/vision/YellowBlockAnaylzer.java
@@ -7,6 +7,7 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
@@ -19,6 +20,8 @@ public class YellowBlockAnaylzer {
     private Mat image;
     private final static Scalar lowerBounds = new Scalar(18, 134, 45);
     private final static Scalar upperBounds = new Scalar(30, 255, 255);
+
+    private final static int CROP_AMT = 300;
 
     // outdated values
 //    private final static Scalar lowerBounds = new Scalar(22, 81, 0);
@@ -33,17 +36,21 @@ public class YellowBlockAnaylzer {
 
     public YellowBlockResult process() {
         Log.i(TAG, "Starting to image process ball");
-        Mat hsvImage = image.clone();
-        Mat hierachy = image.clone();
-        Mat thresholded = image.clone();
+
+        // crop image
+        Rect croppedArea = new Rect(0, 0, image.width(), image.height());
+        Mat croppedImage = new Mat(image, croppedArea);
+
+        Mat hsvImage = croppedImage.clone();
+        Mat hierachy = croppedImage.clone();
+        Mat thresholded = croppedImage.clone();
 
         List<MatOfPoint> contours = new ArrayList<>();
 
-
         // erode and dilate to remove noise
         Mat kernel = Mat.ones(5, 5, CvType.CV_8U);
-        Mat morphedImage = new Mat(image.size(), image.type());
-        Imgproc.morphologyEx(image, morphedImage, Imgproc.MORPH_OPEN, kernel);
+        Mat morphedImage = new Mat(croppedImage.size(), croppedImage.type());
+        Imgproc.morphologyEx(croppedImage, morphedImage, Imgproc.MORPH_OPEN, kernel);
 
         Imgproc.cvtColor(morphedImage, hsvImage, Imgproc.COLOR_RGB2HSV);
 
@@ -67,15 +74,16 @@ public class YellowBlockAnaylzer {
             int centerY = (int) (contourMoments.get_m01() / contourMoments.get_m00());
 
             Point center = new Point(centerX, centerY);
-            Imgproc.circle(image, center, 5, new Scalar(150, 8, 206), 15);
+            Imgproc.circle(croppedImage, center, 5, new Scalar(150, 8, 206), 15);
 
-            return new YellowBlockResult(thresholded, image, matOnlyContours, area, center);
+            return new YellowBlockResult(thresholded, croppedImage, matOnlyContours, area, center);
         } else {
             Log.i(TAG, "No contours found");
 
             hierachy.release();
             hsvImage.release();
             matOnlyContours.release();
+            croppedImage.release();
 
             return new YellowBlockResult(thresholded);
         }
